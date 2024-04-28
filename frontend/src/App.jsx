157b328a3135ffc9d9514b136aaa76b3b3a84393
vishaaal <officialvk02@gmail.com>
2024-04-28 17:26:09 +0530

    setup react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,22 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Criminal from './components/criminal/Criminal'
 import Footer from './components/footer/Footer'
+import About from './About'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
  
 
   return (
-    <>
-        <Navbar/>
-        <Criminal/>
-        <Footer/>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Criminal />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import "./navbar.css";
 import { GiCctvCamera } from "react-icons/gi";
 import useSound from "use-sound"; 
 import Sound from "../../assets/wave.mp3"
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const soundUrl = Sound;
@@ -10,18 +11,21 @@ const Navbar = () => {
 
   return (
     <div className="m-6 flex  font-bold">
+        
       <div className="flex basis-4/6 ">
         <div
           className="text-4xl  underline cursor-pointer "
           onMouseEnter={play} 
         >
-          Security.{" "}
+          <Link to="/">Security.{" "}</Link>
+          
         </div>
         <GiCctvCamera />
       </div>
       <div className="flex text-2xl pt-2 gap-x-12 ">
-        <div className="hover:text-[#676f9d] cursor-pointer" onMouseEnter={play} >Records</div>
-        <div className="hover:text-[#676f9d] cursor-pointer" onMouseEnter={play} >About</div>
+        <div className="hover:text-[#676f9d] cursor-pointer" onMouseEnter={play} ><Link to="/about">Records</Link></div>
+      <div className="hover:text-[#676f9d] cursor-pointer" onMouseEnter={play} ><Link to="/about">About</Link></div>
+        
         <div className="hover:text-[#676f9d] cursor-pointer" onMouseEnter={play} >Notifications</div>
       </div>
       <div className="border-2 rounded-full w-12 h-12 ml-14 cursor-pointer" onMouseEnter={play}></div>
